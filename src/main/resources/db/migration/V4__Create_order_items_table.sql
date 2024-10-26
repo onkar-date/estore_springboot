07870d5e5ec519494a4e54f39f5ede2ad93c6511
onkar-date <onkar101197@gmail.com>
2024-10-26 12:51:11 +0530

    Added sub-orders table and modified order-items table

--- src/main/resources/db/migration/V4__Create_order_items_table.sql
+++ src/main/resources/db/migration/V4__Create_order_items_table.sql
@@ -1,9 +1,12 @@
 CREATE TABLE order_items (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    order_id BIGINT NOT NULL, -- Foreign key to orders table
-    product_id BIGINT NOT NULL, -- Foreign key to products table
-    quantity INT NOT NULL, -- Quantity of the product ordered
-    price INT NOT NULL, -- Price of the product in paise (integer)
+    sub_order_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    price INT NOT NULL,
+    total_price INT NOT NULL,
+    order_id BIGINT NOT NULL,
+    status ENUM('PENDING', 'SHIPPED', 'DELIVERED', 'CANCELLED') NOT NULL,
 
     -- Foreign key constraints
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
